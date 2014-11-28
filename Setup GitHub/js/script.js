8f09f418f07c5cea1e3f34195c4ad450be54b054
Carlos <crarana@fullsail.edu>
2014-11-27 19:02:03 -0500

    Prompted user for name
    Prompted user for name

--- Setup GitHub/js/script.js
+++ Setup GitHub/js/script.js
@@ -4,4 +4,9 @@ Test JS file
 11/27/2014
  */
 
-alert("Testing 1, 2, 3 ")
+//alert("Testing 1, 2, 3 ");
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name:");
+console.log(userInput);
