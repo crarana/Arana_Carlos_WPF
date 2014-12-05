7052fae6e02823acc5d5585d4bc533040f43aca2
Carlos <crarana@fullsail.edu>
2014-12-04 20:50:12 -0500

    Added prompt for personal
    Added prompt to explain personal expression and added the first
    variable.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,4 +3,6 @@
 // Activity: Expressions Worksheet
 // Student ID: #0004645079
 
+alert("My personal expressions exercise revolves around the amount of money I spend annually on tolls.");
 
+var tollCost = prompt("How much is the toll cost?");
