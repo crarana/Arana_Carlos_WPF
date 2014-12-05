ee858fb3f75da33cc109d35b22f313f8cb641214
Carlos <crarana@fullsail.edu>
2014-12-04 21:50:30 -0500

    EI Added prompt comment
    Added prompt as a comment to my Industry Expressions script.js file.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,10 @@
  Activity: Expressions Worksheet
  Student ID: #0004645079
 */
+
+/*
+For this expressions practice I decided to make it about my current job. It is holiday season and we are offering great
+deals on our high-end line of haircare products. A lot of money goes into the research and development of Loreal's
+Kerastase line of products and the pricepoint for the products indicates that. Because the products are expensive,
+the holiday season is a great time to offer discounts that make them more affordable.
+ */
