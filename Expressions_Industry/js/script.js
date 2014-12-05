c70e2d7f3e00ce4716b3ae697f1d5a9df5c6122c
Carlos <crarana@fullsail.edu>
2014-12-04 22:05:51 -0500

    EI Defined the variable bundle
    EI Defined the variable bundle

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -21,4 +21,4 @@ var itemPrices = [prompt("What is the original price of the first product?"), pr
 
 var discountItems = prompt("What is the percentage discounted of buying individual products this holiday season?"); //To determine the percent off for individual items
 
-var bundle = ;
+var bundle = prompt("What is the original price of the 3 item bundle?");
