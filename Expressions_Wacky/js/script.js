c3bbc25ef59abcb2ced10f13ef3ee4ed39a0efa7
Carlos <crarana@fullsail.edu>
2014-12-04 23:08:15 -0500

    EW comment added to balance var prompt
    EW comment added to balance var prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,7 +10,7 @@
 
 alert("In this expressions project, we will consider the interest applied to a credit card with certain purchases on it at 12% interest per year. You will be asked for the initial balance on the card at the beginning of month one, then for the 3 purchases made on the card in month 1, then the 3 purchases made in month 2, and finally the three purchases made in month 3.") //alert to indicate the exercise
 
-var initBalance = prompt("What is the initial balance on the card?");
+var initBalance = prompt("What is the initial balance on the card?"); //gather the given for the initial balance
 var purchaseA = [prompt("What is the first purchase made in the first month?"), ("What is the second purchase made in the first month?"), ("What is the third purchase made in the first month?") ];
 var purchaseB = [];
 var purchaseC = [];
