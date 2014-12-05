056e334da55a91a671caf1179545b22bb171813b
Carlos <crarana@fullsail.edu>
2014-12-04 23:37:14 -0500

    EW adjustments made to language of alerts
    EW adjustments made to language of alerts

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -28,21 +28,14 @@ percent++;
 var monthTotal = Number(initBalance) + Number(purchaseA[0]) + Number(purchaseA[1]) + Number(purchaseA[2]); //First month total before percentage applied
 var balanceTotal = monthTotal * percent; //Balance for first month + interest
 
-alert(" The total is" + " " + balanceTotal + ". "); //First month balance
+alert(" The total for month one including interest is" + " " + balanceTotal + ". "); //First month balance
 
 balanceTotal += Number(purchaseB[0]) + Number(purchaseB[1]) + Number(purchaseB[2]); //Balance of second month before interest
 balanceTotal *= percent; //Balance of second month after interest 
 
-alert(" The total is" + " " + balanceTotal + ". "); //Second month balance
+alert(" The total for month two including interest is" + " " + balanceTotal + ". "); //Second month balance
 
 balanceTotal += Number(purchaseC[0]) + Number(purchaseC[1]) + Number(purchaseC[2]); //Balance of third month before interest
 balanceTotal *= percent; //Balance of third month after interest 
 
-alert(" The total is" + " " + balanceTotal + ". "); //third month balance
-
-
-
-
-
-alert(" The total is" + " " + balanceTotal + ". ");
-
+alert(" The total for month three including interest is" + " " + balanceTotal + ". "); //third month balance
