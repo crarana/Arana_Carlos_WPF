f1ead93791ab2983d0271e32d7745723421f928c
Carlos <crarana@fullsail.edu>
2014-12-04 23:07:52 -0500

    EW comment added to initial alert
    EW comment added to initial alert

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,7 +8,7 @@
 
 /* compounded monthly interest for a credit card with an annual interest rate of 12%. */
 
-alert("In this expressions project, we will consider the interest applied to a credit card with certain purchases on it at 12% interest per year. You will be asked for the initial balance on the card at the beginning of month one, then for the 3 purchases made on the card in month 1, then the 3 purchases made in month 2, and finally the three purchases made in month 3."
+alert("In this expressions project, we will consider the interest applied to a credit card with certain purchases on it at 12% interest per year. You will be asked for the initial balance on the card at the beginning of month one, then for the 3 purchases made on the card in month 1, then the 3 purchases made in month 2, and finally the three purchases made in month 3.") //alert to indicate the exercise
 
 var initBalance = prompt("What is the initial balance on the card?");
 var purchaseA = [prompt("What is the first purchase made in the first month?"), ("What is the second purchase made in the first month?"), ("What is the third purchase made in the first month?") ];
