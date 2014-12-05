29fdf2fb40e4a0ab6abe330c2f0061b0e7309feb
Carlos <crarana@fullsail.edu>
2014-12-04 21:33:53 -0500

    Change EP script.js comments
    Changed EP script.js comments to group them together on the top where I
    put my name, the date, etc.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,9 @@
-// Name: Carlos Arana
-// Date: 12/4/14
-// Activity: Expressions Worksheet
-// Student ID: #0004645079
+/*
+ Name: Carlos Arana
+ Date: 12/4/14
+ Activity: Expressions Worksheet
+ Student ID: #0004645079
+*/
 
 alert("My personal expressions exercise revolves around the amount of money I spend annually on tolls.");
 
