0dc43e15b06919a0a8bc461041ec69d6f562ff6e
Carlos <crarana@fullsail.edu>
2014-12-11 18:39:17 -0500

    As:C added first var
    Added first var

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,3 +8,5 @@
 
 alert("In this program you will be asked to prioritize your next three bills, and subsequently: your checking account balance. With this information the program will determine how many of the bills will be paid based on their priority. None of these bills are due this week.") //alert the user on the upcoming prompts
 
+//acquire givens
+var rent = prompt("How much is your rent?"); //prompt to acquire value for var rent
