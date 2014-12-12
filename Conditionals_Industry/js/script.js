72d41e679b90814cb557959e57636456cf9269d2
Carlos <crarana@fullsail.edu>
2014-12-11 19:46:30 -0500

    As:C added comments and completed Industry
    As:C added comments and completed Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,7 +9,7 @@
 alert("If you buy three items or more, the salon offers 15% off the total."); //alerts the user of the upcoming prompts
 
 //acquire givens
-var total = prompt("How much is the total for your services today?"); //acquire the total for var total
+var total = prompt("How much is the total today?"); //acquire the total for var total
 var items = prompt("How many items are you buying today?"); //acquire the amount of items for the item var
 
 //adjustments
@@ -17,13 +17,13 @@ var total = Number(total); //adjust to numerical value
 var items = Number(items); //adjust to numerical value
 var discount = total*.85; //adjust for discount from total
 
-if(items >= 3) {
-	console.log("Your total for today is" + " $" + discount + ", after a 15% discount.");
-	alert("Your total for today is" + " $" + discount + ", after a 15% discount.");
-}else if (items <=2) {
-	console.log("Your total for today is" + " $" + total + ".");
-	alert("Your total for today is" + " $" + total + ".");
-}else{
+if(items >= 3) { //if 3 or more items, then print with total with discount
+	console.log("Your total for today is" + " $" + discount + ", after a 15% discount."); //console log
+	alert("Your total for today is" + " $" + discount + ", after a 15% discount."); //alert the user
+}else if (items <=2) { //if less than 3 items, then print total
+	console.log("Your total for today is" + " $" + total + "."); //console log
+	alert("Your total for today is" + " $" + total + "."); //alert log
+}else{ //in any other case
 	console.log("Did not have enough information to complete the calculation. Please try again."); //console log
 	alert("Did not have enough information to complete the calculation. Please try again."); //alert the user
 };
