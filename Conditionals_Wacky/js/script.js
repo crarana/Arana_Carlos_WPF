113966decf5796968e5f0fcba8e8f9f1acc46bd2
Carlos <crarana@fullsail.edu>
2014-12-11 20:03:06 -0500

    As:C Added comments
    Added comments to wacky project

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,12 +6,14 @@
 
 //This prompt will be to determine what tiles are options for the project of laying in new tile floor for a room.
 
-alert("The cost of wood floor tiles is $.75 each. The cost of ceramic floor tiles is $1.25 each. You are going to be laying new tile and you'd like to know which is within your budget. Each tile is one foot by one foot.");
+alert("The cost of wood floor tiles is $.75 each. The cost of ceramic floor tiles is $1.25 each. You are going to be laying new tile and you'd like to know which is within your budget. Each tile is one foot by one foot."); //alert the user to the coming prompts
 
-var length = prompt("What is the length of the room in feet?");
-var width = prompt("What is the width of the room in feet?");
-var budget = prompt("What is your budget for this project?");
+//acquire values with given prompts
+var length = prompt("What is the length of the room in feet?"); //acquire a value for the length var
+var width = prompt("What is the width of the room in feet?"); //acquire a value for the width var
+var budget = prompt("What is your budget for this project?"); //acquire a value for the budget
 
-var length = Number(length);
-var width = Number(width);
-var budget = Number(budget);
+//adjustments
+var length = Number(length); //adjust value for length to a numeric value
+var width = Number(width); //adjust value for width to a numeric value
+var budget = Number(budget); //adjust value for budget to a numeric value
