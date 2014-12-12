1fa6d95a3c1ca0b37c6f1f0e263ad569d85598d3
Carlos <crarana@fullsail.edu>
2014-12-11 19:57:49 -0500

    As:C changed the prompt for wacky and added to it
    As:C changed the prompt for wacky and added to it

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,3 +4,14 @@
 // Project: Wacky conditionals
 // Student ID: #0004645079
 
+//This prompt will be to determine what tiles are options for the project of laying in new tile floor for a room.
+
+alert("The cost of wood floor tiles is $.75 each. The cost of ceramic floor tiles is $1.25 each. You are going to be laying new tile and you'd like to know which is within your budget. Each tile is one foot by one foot.");
+
+var length = prompt("What is the length of the room in feet?");
+var width = prompt("What is the width of the room in feet?");
+var budget = prompt("What is your budget for this project?");
+
+var length = Number(length);
+var width = Number(width);
+var budget = Number(budget);
