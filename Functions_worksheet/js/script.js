5fb07f581b7eca95b212fac77b2c1e3b406a4a00
Carlos <crarana@fullsail.edu>
2014-12-18 20:35:46 -0500

    FW: Added Comment
    FW: Added Comment

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -19,7 +19,7 @@ function calculateCircumference(radius){ //function, and functionname
 
 //It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function.
 var weight= prompt("How much is the weight of the victim?"); //var weight defined by prompt results
-var kill = calculateKill(weight);
+var kill = calculateKill(weight); //var kill defined by function caclculateKill
 console.log("It would take" + " " + kill + " " + "beestings to kill an animal that weighs" + " " + weight + "pounds");
 
 function caclculateKill(weight){
