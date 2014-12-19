3c589aeab75c673951c2af7549b2d9ef3345e9c9
Carlos <crarana@fullsail.edu>
2014-12-18 22:17:21 -0500

    FPP: Added Comments
    FPP: Added Comments

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -11,16 +11,22 @@
 
 alert("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area."); //alert to let the user know whats coming next
 
+//gathering givens
+
 var width=prompt("What is the width in feet of the room?"); //var for width of room given
 var length=prompt("What is the length in feet of the room?"); //var for length of room given
 var walkWidth=prompt("What is the width in feet of the intended walkway?"); //var for walkwidth given
 var walkLength=prompt("What is the length in feet of the intended walkway?"); //var for walklength given
+
+//declaring vars with functions as definitions
 
 var roomArea = calculateRoom(length, width); //var defined by function with parameters
 var walkArea = calculateWalk(walkWidth, walkLength); //var defined by function with parameters
 var gardenArea = differenceArea(roomArea, walkArea); //var defined by function with parameters
 console.log("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet."); //print to console
 alert("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet."); //alert to the user
+
+//functions 
 
 function calculateRoom(width, length){ //function for calculating room area with given arguments
 	return width*length
