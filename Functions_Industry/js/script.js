db9a5fe9d7560de986228e6f14dc77329e4331bb
Carlos <crarana@fullsail.edu>
2014-12-18 22:24:18 -0500

    FFI: Alert the user and comment
    FFI: Alert the user and comment

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,3 +6,4 @@
 
 //for this one, im deciding to make it about getting a discount on your ice cream for every 5th visit, and an extra special discount for every the 20th visit
 
+alert("You are visiting a cookie shop you often visit. They have a card for frequent visitors where you get a stamp on your card with each visit. They offer special discounts for each 5th visit, and an extra special one when your card is full. A card can hold up to 20 punches.") //alert the user of the coming prompts.
