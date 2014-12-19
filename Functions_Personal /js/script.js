43dabe62d4f40d4ac807f1fa9341371ae62a1c96
Carlos <crarana@fullsail.edu>
2014-12-18 22:12:48 -0500

    FPP: fixed typos in functionnames
    FPP: fixed typos in functionnames

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -9,7 +9,7 @@
 //my roomate and I are building an indoor garden to occupy an empty room in the house. We want the entire room to be a garden except for a walkway.
 //The prompts will be gathered by the user to determine their indoor garden space
 
-prompt("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area.");
+alert("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area.");
 
 var width=prompt("What is the width in feet of the room?");
 var length=prompt("What is the length in feet of the room?");
@@ -20,14 +20,14 @@ var roomArea = calculateRoom(length, width);
 var walkArea = calculateWalk(walkWidth, walkLength);
 var gardenArea = differenceArea(roomArea, walkArea);
 console.log("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet.");
-result("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet.");
+alert("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet.");
 
-function roomArea(width, length){
+function calculateRoom(width, length){
 	return width*length
 };
-function walkArea(walkWidth, walkLength){
+function calculateWalk(walkWidth, walkLength){
 	return walkWidth*walkLength
 };
-function gardenArea(roomArea, walkArea){
+function differenceArea(roomArea, walkArea){
 	return roomArea - walkArea
 };
