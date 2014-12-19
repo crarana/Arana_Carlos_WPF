c71d0ecdb273dfe4e0ec761a7825214933e9ffcb
Carlos <crarana@fullsail.edu>
2014-12-18 22:22:21 -0500

    FFI: Added Functions Industry Folder
    FFI: Added Functions Industry Folder

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,8 @@
+// Name: Carlos Arana
+// Date: 12/18/14
+// Assignment: Functions
+// Project: Functions Industry
+// Student ID: #0004645079
+
+//for this one, im deciding to make it about getting a discount on your ice cream for every 5th visit, and an extra special discount for every the 20th visit
+
