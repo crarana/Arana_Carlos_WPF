b24f18c18acadea73a29ce99509e1cb1715129e6
Carlos <crarana@fullsail.edu>
2014-12-18 22:13:04 -0500

    FPP: Added Comment
    FPP: Added Comment

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -9,7 +9,7 @@
 //my roomate and I are building an indoor garden to occupy an empty room in the house. We want the entire room to be a garden except for a walkway.
 //The prompts will be gathered by the user to determine their indoor garden space
 
-alert("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area.");
+alert("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area."); //alert to let the user know whats coming next
 
 var width=prompt("What is the width in feet of the room?");
 var length=prompt("What is the length in feet of the room?");
