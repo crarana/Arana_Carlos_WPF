9ac266eae47bb44eedc66d6a47654db78e6d8bc5
Carlos <crarana@fullsail.edu>
2014-12-18 22:10:20 -0500

    FPP Added the console log and result, fixed typos
    FPP Added the console log and result and fixed typos

--- Functions_Personal /js/script.js
+++ Functions_Personal /js/script.js
@@ -11,14 +11,16 @@
 
 prompt("You will be converting an empty room in your house to an indoor garden. To do so, you must consider how much space wll be devoted to a walkway and how much space will be devoted to the garden area.");
 
-var width=prompt("What is the width of the room?");
-var length=prompt("What is the length of the room?");
-var walkWidth=prompt("What is the width of the intended walkway?");
-vat walkLength=prompt("What is the length of the intended walkway?");
+var width=prompt("What is the width in feet of the room?");
+var length=prompt("What is the length in feet of the room?");
+var walkWidth=prompt("What is the width in feet of the intended walkway?");
+var walkLength=prompt("What is the length in feet of the intended walkway?");
 
 var roomArea = calculateRoom(length, width);
 var walkArea = calculateWalk(walkWidth, walkLength);
 var gardenArea = differenceArea(roomArea, walkArea);
+console.log("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet.");
+result("If the room is" + " " + roomArea + " " + "square feet and the walkway area is" + " " + walkArea + " " + "square feet, then the garden can be" + " " + gardenArea + " " + "square feet.");
 
 function roomArea(width, length){
 	return width*length
